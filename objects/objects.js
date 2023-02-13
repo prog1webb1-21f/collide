a0716fda080b5ae46b28d617b7efa130f81c53cf
Johan Kellen <johan.kellen@linkoping.se>
2023-02-13 16:01:53 +0100

    objects klar

--- objects/objects.js
+++ objects/objects.js
@@ -7,84 +7,100 @@
 let smileys = [];
 let player;
 
-
 function setup() {
-	createCanvas(400,400);
-	for(let i=0; i<3; i++){
-		smileys.push(new Smiley(i*75+25, 50));
-	}
-	console.table(smileys);
-	player = new Player();
+  createCanvas(400, 400);
+  for (let i = 0; i < 3; i++) {
+    smileys.push(new Smiley(i * 125 + 25, 50));
+  }
+  console.table(smileys);
+  player = new Player();
 }
 
-function draw(){
-	background(220);
-	
-	player.update();
-	player.draw();
+function draw() {
+  background(220);
 
-	for (let i=0; i<smileys.length; i++){
-		player.scares(smileys[i]);
-		if(player.eats(smileys[i])){
-			console.log(smileys[i]);
-			smileys.splice(i,1);
-			break;
-		}
-		smileys[i].draw();
-	}	
-	
+  player.update();
+  player.draw();
+
+  for (let i = 0; i < smileys.length; i++) {
+    player.scares(smileys[i]);
+    if (player.eats(smileys[i])) {
+      console.log(smileys[i]);
+      smileys.splice(i, 1);
+      break;
+    }
+    smileys[i].draw();
+  }
 }
 
 function preload() {
-	emojiHappy = loadImage('assets/emojiHappy.png');
-	emojiSad = loadImage('assets/emojiSad.png');
-  }
-
-class Smiley {
-	constructor(x,y){
-		this.x = x;
-		this.y = y;
-		this.d = 20;
-		this.isHappy = true;
-	}
-	draw(){
-		if(this.isHappy){
-			image(emojiHappy,this.x,this.y,this.d,this.d);
-		} else {
-			image(emojiSad,this.x,this.y,this.d,this.d);
-		}
-		
-	}
-	
+  greenBox = loadImage("assets/box-soft-green.png");
+  pinkBox = loadImage("assets/box-pink.png");
 }
 
-class Player{
-	constructor(){
-		this.x = width/2;
-		this.y = height/2;
-		this.d = 20;
-	}
-	update(){
-		this.x = mouseX;
-		this.y = mouseY;
-	}
-	draw(){
-		circle(this.x, this.y, this.d);
-	}
-	scares(smiley){
-		if(collideRectCircle(smiley.x,smiley.y,smiley.d,smiley.d,this.x,this.y,this.d*5)){	
-			smiley.isHappy = false;
-			return true;
-		} else {
-			smiley.isHappy = true;
-			return false;
-		}		
-	}
-	eats(smiley){
-		if(collideRectCircle(smiley.x,smiley.y,smiley.d,smiley.d,this.x,this.y,this.d)){	
-			return true;
-		} else {
-			return false;
-		}
-	}
+class Smiley {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.d = 100;
+    this.isHappy = true;
+  }
+  draw() {
+    if (this.isHappy) {
+      image(greenBox, this.x, this.y, this.d, this.d);
+    } else {
+      image(pinkBox, this.x, this.y, this.d, this.d);
+    }
+  }
+}
+
+class Player {
+  constructor() {
+    this.x = mouseX;
+    this.y = mouseY;
+    this.d = 20;
+  }
+  update() {
+    this.x = mouseX;
+    this.y = mouseY;
+  }
+  draw() {
+    circle(this.x, this.y, this.d);
+  }
+  scares(smiley) {
+    if (
+      collideRectCircle(
+        smiley.x,
+        smiley.y,
+        smiley.d,
+        smiley.d,
+        this.x,
+        this.y,
+        this.d * 5
+      )
+    ) {
+      smiley.isHappy = false;
+      return true;
+    } else {
+      smiley.isHappy = true;
+      return false;
+    }
+  }
+  eats(smiley) {
+    if (
+      collideRectCircle(
+        smiley.x,
+        smiley.y,
+        smiley.d,
+        smiley.d,
+        this.x,
+        this.y,
+        this.d
+      )
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
